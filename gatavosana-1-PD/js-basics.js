650a332624f761af468209a1d39bbe47d45c9c6a
unknown <Niks@gmail.com>
2024-11-20 16:30:54 +0200

    first commit

---
+++ gatavosana-1-PD/js-basics.js
@@ -0,0 +1,57 @@
+//variebles
+
+//let firstName = "jānis"
+//console.log(firstName);
+//let myArray = [1,2,3,4,];
+
+// conditionals
+
+//let x = 10;
+//if (x == 10 ){
+//    console.log("x ir vienāds ar 10");
+//}
+
+//let person = {
+   // vards: "antra",
+   // irStudents: true,
+//}
+//console.log(person.irStudent); // dot natation
+//console.log(person.vards); // dot natation
+
+//let attribute = "vards";
+//console.log(person[attribute]);
+//console.log(person);
+//person.vecums = 16;
+//console.log(person);
+
+// ternary operator
+//let ageStatus = person.vecums >=18 ? "Pilngadīgs" : "Nepilngadīgs"
+//console.log(ageStatus);
+
+//let namesArray = ["Antra", "Ilmārs", "Rēnārs", "Pauls"] 
+//indeksi ir attiecīgi [0,1,2,3]
+//namesArray[2];
+
+//for (let i = 0; i < namesArray.length; i++){
+    //console.log(namesArray[i]);
+//}
+
+function getReactArea() {
+    return 3 * 4;
+}
+let result = getReactArea();
+console.log(result);
+function getReactAreaAdvanced(width, height) {
+    return width * height;
+}
+console.log(getReactAreaAdvanced(5, 6));
+
+//const printWholeNumbers = function() {
+//    return "svieki";
+//};
+//console.log(printWholeNumbers());
+
+//funtion with return value or without
+//function drive() {
+   // console.log(result);
+//}
